9c77c39bdee7b3828158989c1b3c8ef0e2a3a6c9
Deiaa Mohamed <moamn100g@gmail.com>
2025-02-22 10:35:09 +0200

    update the salary by searching name feature has been added

--- Employees_Data_Holder.py
+++ Employees_Data_Holder.py
@@ -5,7 +5,7 @@ class Employee:
         self.age=Age
         self.Salary=Salary
     def __str__(self):
-        return f"Name: {self.name}|| Age: {self.age}|| Salary: {self.Salary}"
+        return f"Name: {self.name} || Age: {self.age} || Salary: {self.Salary}"
 class EmployeesManager:
    
     def __init__(self):
@@ -16,8 +16,12 @@ class EmployeesManager:
         self.emplst.append(emp)
    
     def delete_emp(self,sage,lage):
-        self.emplst=[snapshot for snapshot in self.emplst if snapshot[0].age not in range(sage,lage)]
-                
+        self.emplst=[snapshot for snapshot in self.emplst if snapshot.age not in range(sage,lage)]
+    
+    def update_salary_by_age(self,name,salary):
+        for item in self.emplst:
+            if name==item.name:
+                item.Salary=salary
     
     def __str__(self):
         result=[]
@@ -29,15 +33,18 @@ emp=EmployeesManager()
 while True:
     print("Program Options:\n"
       "(1) Add Employee\n"
-      "(2) Delete Employee(under test request)\n"
+      "(2) Update Salary Employee(under test request)\n"
       "(3) Update Employee by age range(under test request)\n"
       "(4) Show Employee Data Base\n"
-      "(5) End the Program\n\n")
+      "(5) End the Program\n")
     choosed=int(input())
     if choosed in range(1,6):
         if choosed==1:
             emp.save_emps(input("Enter Employee Name: "),int(input("Enter Employee Age: ")),int(input("Enter Employee Salary: ")))
-        
+            print("Employee Added Successfully\n")
+        elif choosed==2:
+            emp.update_salary_by_age(input("Enter the Name: "),int(input("Enter the New Salary: ")))
+            print("Salary Changed Successfuly\n")
         elif choosed==3:
             emp.delete_emp(int(input("Enter the first of the range: ")),int(input("Enter the last of the range: ")))
             
@@ -45,5 +52,7 @@ while True:
             print(str(emp))
         elif choosed==5:
             break
+    else:
+        print("Invalid!, Choose between 1 to 5\n")
 
         
